ba2f73be02cadfa7abbdd3c3b5a8519d6cd44efe
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-05 20:43:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+  var counter = 0;
+
+   function addBump(){
+    counter++
+  }
+
+   function getBumps(){
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+
+  return function(deadlyDevice){
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
